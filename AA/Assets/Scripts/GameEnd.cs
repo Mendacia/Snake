d756d505c3bf49125034dc62add9afd6a5272607
Liam <liamh2000@gmail.com>
2020-03-11 10:04:36 +1300

    Minimum Viable Product
    The game is very VERY technically completable

---
+++ AA/Assets/Scripts/GameEnd.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameEnd : MonoBehaviour
+{
+
+    private int snakesCollected = 0;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (snakesCollected == 7)
+        {
+            SceneManager.LoadScene("winScreen", LoadSceneMode.Single);
+        }
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            snakesCollected++;
+        }
+    }
+}
